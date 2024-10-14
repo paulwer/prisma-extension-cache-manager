f30cac7ef81915fce1b90eef1b701be9e225f9b1
Paul Werner <paul.werner@almig.de>
2024-10-14 08:26:13 +0200

    chore: add example repo

---
+++ example/index.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client';
+import * as cm from 'cache-manager';
+import assert from 'node:assert';
+import cacheExtension from '@paulwer/prisma-extension-cache-manager';
+
+async function main() {
+  const cache = await cm.caching('memory', {
+    ttl: 1000,
+    max: 200,
+  });
+  const prisma = new PrismaClient().$extends(cacheExtension({ cache }));
+  assert(prisma.$cache === cache);
+  await prisma.user.findMany({
+    take: 20,
+    cache: true,
+  });
+  await prisma.user.findUnique({
+    where: {
+      id: 1,
+    },
+    cache: 10 * 1000,
+  });
+  await prisma.user.count({
+    where: {
+      id: {
+        gt: 0,
+      },
+    },
+    cache: {
+      ttl: 30 * 1000,
+      key: `user_count_gt_0`,
+    },
+  });
+}
+
+main().catch(console.error);
